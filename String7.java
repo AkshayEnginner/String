24b92c0b5e075949a001c2bae3078a203e57f0fe
Akshay Gaikwad <132028709+AkshayEnginner@users.noreply.github.com>
2023-05-06 07:00:17 +0530

    Add files via upload

---
+++ String7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class String7
+{
+    public static void main(String args[])
+    {
+       String str = "aks hay";
+       String newstr;
+       newstr=str.replace(" ","x");
+       System.out.println(newstr);
+    }
+}
