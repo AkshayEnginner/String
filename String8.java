5763c9332040eed2ebfce33c3452cd7b391d619e
Akshay Gaikwad <132028709+AkshayEnginner@users.noreply.github.com>
2023-05-06 07:00:33 +0530

    Add files via upload

---
+++ String8.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class String8
+{
+    public static void main(String args[])
+    {
+        String str;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        str = sc.nextLine();
+        System.out.println(str.toUpperCase());
+    }
+}
