cfb0d3bf4102833c58ea8ff5cea39fc1d1e048d6
Akshay Gaikwad <132028709+AkshayEnginner@users.noreply.github.com>
2023-05-07 10:28:53 +0530

    Add files via upload

---
+++ String16.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class String16
+{
+    public static void main(String args[])
+    {
+        String str1 = "akshay";
+        String str2 = "king";
+        
+       System.out.println(str1.concat(str2));
+        
+       
+    }
+}
